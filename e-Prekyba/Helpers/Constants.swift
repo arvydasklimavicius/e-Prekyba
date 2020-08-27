324edb5467e1d8d38e8e3059e287a8f410f1ce85
Arvydas Klimavicius <arvydas.klimavicius@icloud.com>
2020-08-27 17:55:26 +0300

    Creating Basket Class, download and save to Firestore functions created

--- e-Prekyba/Helpers/Constants.swift
+++ e-Prekyba/Helpers/Constants.swift
@@ -11,25 +11,28 @@ import Foundation
 //MARK: - ID's and Keys
 public let cFILEREFERENCE = "gs://e-prekyba-d6460.appspot.com"
 
-//MARK: Category
+//MARK: - Category
 
 public let cNAME = "name"
 public let cIMAGENAME = "imageName"
 public let cOBJECTID = "objectId"
 
-//MARK: Firebase Headers
+//MARK: - Firebase Headers
 
 public let cUSER_PATH = "User"
 public let cCATEGORY_PATH = "Category"
 public let cITEMS_PATH = "Items"
 public let cBASKET_PATH = "Basket"
 
-//MARK: Item
+//MARK: - Item
 public let cCATEGORYID = "categoryId"
 public let cDESCRIPTION = "description"
 public let cPRICE = "price"
 public let cIMAGELINKS = "imageLinks"
 
+//MARK: - Basket
+public let cOWNERID = "ownerId"
+public let cITEMSID = "itemIds"
 
 
 
---
+++ e-Prekyba/Model/Basket.swift
@@ -0,0 +1,52 @@
+//
+//  Basket.swift
+//  e-Prekyba
+//
+//  Created by Arvydas Klimavicius on 2020-08-27.
+//  Copyright © 2020 Arvydas Klimavicius. All rights reserved.
+//
+
+import Foundation
+
+class Basket {
+    var id: String!
+    var ownerId: String!
+    var itemIds: [String]!
+    
+    init() {
+    }
+    
+    init(_dictionary: NSDictionary) {
+        id = _dictionary[cOBJECTID] as? String
+        ownerId = _dictionary[cOWNERID] as? String
+        itemIds = _dictionary[cITEMSID] as? [String]
+    }
+    
+}
+//MARK: DOwnload basket from Firestore
+func downloadBasketFromFirestore(_ ownerId: String, completion: @escaping (_ basket: Basket?) -> ()) {
+    FirebaseReferrence(.Basket).whereField(cOWNERID, isEqualTo: ownerId).getDocuments { (snapshot, error) in
+        guard let snapshot = snapshot else {
+            completion(nil)
+            return
+        }
+        if !snapshot.isEmpty && snapshot.documents.count > 0 {
+            let basket = Basket(_dictionary: snapshot.documents.first!.data() as NSDictionary)
+            completion(basket)
+        } else {
+            completion(nil)
+        }
+    }
+}
+
+//MARK: - Save Basket to Firestore
+
+func saveCartToFirestore(_ basket: Basket) {
+    FirebaseReferrence(.Basket).document(basket.id).setData(basketDictionaryFrom(basket) as! [String: Any])
+}
+
+//MARK: - Helper Functions
+
+func basketDictionaryFrom(_ basket: Basket) -> NSDictionary {
+    return NSDictionary(objects: [basket.id, basket.itemIds, basket.ownerId], forKeys: [cOBJECTID as NSCopying, cITEMSID as NSCopying, cOWNERID as NSCopying])
+}
